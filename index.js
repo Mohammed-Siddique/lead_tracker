86a97cebbe459c5d461f063d809a3a0e2b82346f
Mohammed Siddique <khanshahid9066@gmail.com>
2025-03-20 20:56:33 +0530

    lead_tracker
    Use createElement() & append() instead of innerHTML

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-let myLeads = ['www.awesomelead.com', 'www.greatlead.com']
+let myLeads = []
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    renderLeads()
 })
 
-let listItems = ""
+function renderLeads() {
+    let listItems = ""
 
-for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>"
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+        console.log(listItems)
+    }
 
+    ulEl.innerHTML = listItems
 }
 
-ulEl.innerHTML = listItems
+
 
