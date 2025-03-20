84b29ecf7845c147117711c7b95edc893da850a3
Mohammed Siddique <khanshahid9066@gmail.com>
2025-03-20 20:44:24 +0530

    lead_tracker
    Use createElement() & append() instead of innerHTML

--- index.js
+++ index.js
@@ -10,4 +10,8 @@ inputBtn.addEventListener("click", function() {
 
 for (let i = 0; i < myLeads.length; i++) {
     ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+
+    const li = document.createElement(li)
+    
 }
+
