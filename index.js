d1b4182fec89a513568bf1db1d875ed4ad366aab
Mohammed Siddique <khanshahid9066@gmail.com>
2025-03-22 22:03:56 +0530

    lead_tracker
    Use createElement() & append() instead of innerHTML

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ function renderLeads() {
     let listItems = ""
 
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li><a target = '_blank' href = ' "+ myLeads[i]+" ' >" + myLeads[i] + "</li></a>"
+        // listItems += "<li><a target = '_blank' href = ' "+ myLeads[i]+" ' >" + myLeads[i] + "</li></a>"
+        listItems += `<li>
+                        <a target = '_blank' href = '${myLeads[i]}' >
+                            ${myLeads[i]}
+                        </a>
+                    </li>`
         console.log(listItems)
     }
 
